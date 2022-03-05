a967a1a886df13833e693adb6b1078762f692c7f
Ahmed Khaled Hilal <Hillaal@github.com>
2022-03-05 22:13:09 +0200

    .

--- Tasks/task_01/code/Core/Src/TIMER.c
+++ Tasks/task_01/code/Core/Src/TIMER.c
@@ -2,19 +2,19 @@
 
 
 
-void TIM_Init(unsigned char Mode,unsigned short Prescaler){
+void TIM_Init(unsigned char counterMode,unsigned short prescaler){
 
 	RCC_REG(RCC_APB1ENR) |= 0x01; /*Enable Timer 2 clock*/
 
-	TIM_REG(TIM2,TIMx_PSC) = Prescaler - 1;		/*setting the prescaler value*/
+	TIM_REG(TIM2,TIMx_PSC) = prescaler - 1;		/*setting the prescaler value*/
 
-	TIM_REG(TIM2,TIMx_ARR) = 16000;		/*setting the autoReload value*/
+	TIM_REG(TIM2,TIMx_ARR) = 16000;		/*setting the autoReload value */
 
 	TIM_REG(TIM2,TIMx_CR1) &= ~(0x02 << 5);		/*Edge-aligned mode. The counter counts up or down
 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 depending on the direction bit (DIR).*/
 
 	TIM_REG(TIM2,TIMx_CR1) &= ~(0x01 << 4); /* clearing DIR bit before inserting the new value*/
-	TIM_REG(TIM2,TIMx_CR1) |= (Mode << 4); /* setting DIR bit*/
+	TIM_REG(TIM2,TIMx_CR1) |= (counterMode << 4); /* setting DIR bit*/
 
 
 	/*TIM_REG(TIM2,TIMx_CR1) |= (0x01 << 2);  only overflow and underflow generate update*/
